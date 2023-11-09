387846b5d3da08ade0c4014d98ccde744a199121
shinminchang <tlsalsckd01@naver.com>
2023-11-09 21:24:39 +0900

    myprofile

---
+++ src/MyProfile.java
@@ -0,0 +1,18 @@
+public class MyProfile {
+    public static void main(String[] args) {
+        System.out.println("이름 : 신민창");
+        System.out.println("성별 : 남");
+        System.out.println("나이 : 32");
+
+        System.out.println("**********");
+        System.out.println("**********");
+        System.out.println("**********");
+        System.out.println("**********");
+        System.out.println("**********");
+        System.out.println("**********");
+        System.out.println("**********");
+        System.out.println("**********");
+        System.out.println("**********");
+        System.out.println("**********");
+    }
+}
